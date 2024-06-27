3741c0357c6edff4f9c39809d0e83f62915ea03b
Harry Gardiner Lunar <harry@lunar.build>
2024-06-27 09:12:01 +0100

    Added live reload plugin for development. Works with 0 setup. Updated woo to latest version.

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,2 +1,12 @@
 <?php
+// Add your custom styles.
+function my_child_theme_styles() {
+    // Enqueue the parent stylesheet.
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
+    // Enqueue your custom stylesheet.
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+}
+
+// Hook into the 'wp_enqueue_scripts' action.
+add_action('wp_enqueue_scripts', 'my_child_theme_styles');
