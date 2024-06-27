776ea652ef84b838d7581ac8338822757a5091f9
Harry Gardiner Lunar <harry@lunar.build>
2024-06-27 09:02:53 +0100

    Added boutique theme

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,6 +1,2 @@
 <?php
-function storefront_child_enqueue_styles() {
-    wp_enqueue_style('storefront-parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('storefront-child-style', get_stylesheet_directory_uri() . '/style.css', array('storefront-parent-style'));
-}
-add_action('wp_enqueue_scripts', 'storefront_child_enqueue_styles');
+
