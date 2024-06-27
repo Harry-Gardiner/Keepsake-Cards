cebf1a2482dcccddb66f19e9cf314c820532f777
Harry Gardiner Lunar <harry@lunar.build>
2024-06-27 09:20:49 +0100

    Added commented code for registering custom JS if needed

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,12 +1,16 @@
 <?php
 // Add your custom styles.
 function my_child_theme_styles() {
-    // Enqueue the parent stylesheet.
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
-    // Enqueue your custom stylesheet.
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
 
-// Hook into the 'wp_enqueue_scripts' action.
 add_action('wp_enqueue_scripts', 'my_child_theme_styles');
+
+// Add your custom scripts.
+// function my_child_theme_scripts() {
+//     wp_enqueue_script('child-custom-script', get_stylesheet_directory_uri() . '/path/file.js', array('jquery'), false, true);
+// }
+
+// add_action('wp_enqueue_scripts', 'my_child_theme_scripts');
