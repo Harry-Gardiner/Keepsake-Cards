8b75dbb68c4a0c8526376f7d6cb0126061c6bac5
Harry Gardiner Lunar <harry@lunar.build>
2024-07-19 15:12:01 +0100

    Created typography file thats imported with use

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -6,7 +6,7 @@ function my_child_theme_styles() {
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array('parent-style'));
 }
 
-add_action('wp_enqueue_scripts', 'my_child_theme_styles');
+add_action('wp_enqueue_scripts', 'my_child_theme_styles', 20);
 
 // Add your custom scripts.
 // function my_child_theme_scripts() {
