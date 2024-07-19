fe6f87a16e3df1554c34f06cf98f2ebfd3de637e
Harry Gardiner Lunar <harry@lunar.build>
2024-07-19 15:00:42 +0100

    Added sass and watch task, which will auto run on opening the project

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -3,7 +3,7 @@
 function my_child_theme_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array('parent-style'));
 }
 
 add_action('wp_enqueue_scripts', 'my_child_theme_styles');
